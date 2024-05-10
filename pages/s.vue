0e6f66adf2decca7219ea09f65c83db0d58193cf
dungTam <dungtamtithoi88@gmail.com>
2024-05-10 22:59:24 +0700

    cemen

--- pages/s.vue
+++ pages/s.vue
@@ -35,10 +35,12 @@ function onMenuToggle(e) {
     <div>
         <div class="n">
 
-            <header class=" relative shadow-lg px-3 py-2">
+            <header class=" relative px-3 py-2 z-20">
                 <nav class="flex justify-between">
                     <div class="w-[130px] md:w-[200px] flex items-center">
                         <img src="https://i.postimg.cc/MZCBXb1K/logo.png" alt="LOGO" srcset="">
+                        <!-- <img src="https://thegioidilac.shop/_nuxt/img/thegioidilac_logo.b7f9124.png" class="h-10" alt="LOGO" srcset=""> -->
+
                     </div>
                     <div class="flex items-center gap-3">
                         <div
